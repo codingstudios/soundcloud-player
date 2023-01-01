9056e979724fa845d3413f9ff45f9c26c85fed9c
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2023-01-02 00:16:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const ytdl = require('ytdl-core');
 const fs = require('fs');
-const yts = require('yt-search');
+const SoundCloud = require("soundcloud-scraper");
+const client = new SoundCloud.Client();
 const ffmpeg = require('fluent-ffmpeg');
 const { exec } = require('child_process');
 var content = ``;
@@ -8,20 +8,33 @@ var content = ``;
 const music = fs.readdirSync('./music').filter(file => file.endsWith('.mp3'));
 const html = fs.readFileSync('./index.html');
 
+const download = (url) => new Promise(async (resolve, reject) => {
+  try {
+    const song = await client.getSongInfo(url);
+    const stream = await song.downloadProgressive();
+    ffmpeg(stream)
+          .audioBitrate(128)
+          .format('mp3')
+          .save(fs.createWriteStream(`./music/${file}`, { flags: 'a' }))
+          .on('end', () => {
+            resolve(`Done ${video?.title}`);
+   })  
+  }catch(e) {
+    reject(e)
+  }
+});
+
 music.forEach(async (file) => {
   try {
-    const video = await yts(file.slice(0, -4));
-    const stream = ytdl(video?.all[0].url, { filter: 'audioonly' });
+    const video = await client.search(file.slice(0, -4));
+    if(!video[0]) throw new Error(`Unable to find ${file.slice(0, -4)}`);
     content += `<audio controls src="./music/${file}"></audio>`;
     fs.writeFileSync("./index.html", content)
     const stats = fs.statSync(`./music/${file}`);
     const fileSizeInBytes = stats.size;
     const size = fileSizeInBytes / (1024*1024);
     if(size > 1) return;
-    ffmpeg(stream)
-          .audioBitrate(128)
-          .format('mp3')
-          .save(fs.createWriteStream(`./music/${file}`, { flags: 'a' }));
+    await download(video[0]?.url);
   }catch(e) {console.log(e)}
 })
 
